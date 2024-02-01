0543e85e09ba3b02cf3c420bb7cea095bffa59f5
Liran Tal <liran.tal@gmail.com>
2024-02-01 15:46:39 +0200

    feat: add example of test suites and error assertions

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,8 +1,19 @@
-import { test } from "node:test";
+import { test, describe } from "node:test";
 import assert from "node:assert";
 import { add } from "../src/math.js";
 
-test("should add two numbers", () => {
-  const result = add(1, 2);
-  assert.strictEqual(result, 3);
+describe("test suite 1", () => {
+  test("should add two numbers", () => {
+    const result = add(1, 2);
+    assert.strictEqual(result, 3);
+  });
+});
+
+describe("test suite 2", () => {
+  test("should fail to add strings", () => {
+    // assert that add() throws an error when called with strings
+    assert.throws(() => {
+      add("1", "2");
+    });
+  });
 });
